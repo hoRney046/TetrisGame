f143e5db49079bd836190fff515d719c2bc5e8ab
horney <osipovsasha290707@gmail.com>
2024-01-21 13:30:43 +0300

    запуск игры

--- main_file.py
+++ main_file.py
@@ -104,4 +104,129 @@ def clear_lines(tetris_grid):
         del tetris_grid[line]
         tetris_grid.insert(0, [None] * tetris_width)
     return len(full_lines)
+# Запуск игры
+def run_game():
+    tetromino, tetromino_color = generate_tetromino()
+    tetromino_x, tetromino_y = tetris_width // 2 - len(tetromino[0]) // 2, 0
+    tetris_grid = [[None] * tetris_width for _ in range(tetris_height)]
 
+    score = 0
+    game_over = False
+
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    if not check_collision(tetromino, tetromino_x - 1, tetromino_y, tetris_grid):
+                        tetromino_x -= 1
+                if event.key == pygame.K_RIGHT:
+                    if not check_collision(tetromino, tetromino_x + 1, tetromino_y, tetris_grid):
+                        tetromino_x += 1
+                if event.key == pygame.K_DOWN:
+                    if not check_collision(tetromino, tetromino_x, tetromino_y + 1, tetris_grid):
+                        tetromino_y += 1
+                if event.key == pygame.K_SPACE:
+                    rotated_tetromino = [[tetromino[y][x] for y in range(len(tetromino))] for x in
+                                         range(len(tetromino[0]) - 1, -1, -1)]
+                    if not check_collision(rotated_tetromino, tetromino_x, tetromino_y, tetris_grid):
+                        tetromino = rotated_tetromino
+
+        if not check_collision(tetromino, tetromino_x, tetromino_y + 1, tetris_grid):
+            tetromino_y += 1
+        else:
+            for y in range(len(tetromino)):
+                for x in range(len(tetromino[y])):
+                    if tetromino[y][x]:
+                        tetris_grid[tetromino_y + y][tetromino_x + x] = tetromino_color
+            cleared_lines = clear_lines(tetris_grid)
+            if cleared_lines > 0:
+                score += 10 * cleared_lines
+            tetromino, tetromino_color = generate_tetromino()
+            tetromino_x, tetromino_y = tetris_width // 2 - len(tetromino[0]) // 2, 0
+            if check_collision(tetromino, tetromino_x, tetromino_y, tetris_grid):
+                game_over = True
+                end_screen()
+
+        screen.fill(WHITE)
+        draw_grid(tetris_grid, tetromino, tetromino_x, tetromino_y, tetromino_color)
+
+        score_text = font.render("Score: " + str(score), True, BLACK)
+        screen.blit(score_text, (20, 20))
+
+        pygame.display.update()
+
+        clock.tick(6)
+
+
+def first_screen():
+    running = True
+    while running:
+        # Обработка событий
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                if button_x <= mouse_pos[0] <= button_x + button_width and \
+                        button_y <= mouse_pos[1] <= button_y + button_height:
+                    # Переход в основную игру
+                    run_game()
+
+        # Очистка экрана
+        screen.fill(WHITE)
+
+        # Отрисовка изображения тетриса
+        tetris_rect = tetris_image.get_rect(center=(200, 140))
+        scale = pygame.transform.scale(
+            tetris_image, (tetris_image.get_width() * 2 + 100,
+                           tetris_image.get_height() * 2 + 100))
+        screen.blit(scale, tetris_rect)
+
+        # Отрисовка кнопки
+        pygame.draw.rect(screen, BLACK, (button_x, button_y, button_width, button_height))
+        screen.blit(button_text, (button_x + 15, button_y + 10))
+
+        # Обновление экрана
+        pygame.display.flip()
+        clock.tick(60)
+
+
+def end_screen():
+    running = True
+    while running:
+        # Обработка событий
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                if button_x <= mouse_pos[0] <= button_x + button_width and \
+                        button_y <= mouse_pos[1] <= button_y + button_height:
+                    # Переход в основную игру
+                    run_game()
+
+        # Очистка экрана
+        screen.fill(WHITE)
+
+        # Отрисовка изображения тетриса
+        tetris_rect = tetris_image.get_rect(center=(200, 140))
+        scale = pygame.transform.scale(
+            tetris_image, (tetris_image.get_width() * 2 + 100,
+                           tetris_image.get_height() * 2 + 100))
+        screen.blit(scale, tetris_rect)
+
+        # Отрисовка кнопки
+        pygame.draw.rect(screen, BLACK, (button_x, button_y, button1_width, button1_height))
+        screen.blit(button1_text, (button_x + 15, button_y + 10))
+
+        # Обновление экрана
+        pygame.display.flip()
+        clock.tick(60)
+
+
+first_screen()
+pygame.quit()
+sys.exit()
